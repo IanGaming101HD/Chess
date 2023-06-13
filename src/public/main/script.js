973f2d053490c8c3bb14608a7f6ed1c387d6b2fe
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2023-06-13 13:12:05 +0100

    a

--- src/public/main/script.js
+++ src/public/main/script.js
@@ -2,75 +2,215 @@ const squares = document.getElementsByClassName('square')
 const positions = Array.from(squares, (square) => square.id)
 let id = 0
 
+function increment(value) {
+    this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'y', 'z'];
+    if (value.toLowerCase().match(/[a-z]/i)) {
+        value = this.alphabet[this.alphabet.indexOf(value.toLowerCase()) + 1]
+        return value
+    } else {
+        value++
+        return value
+    }
+}
+
+function decrement(value) {
+    this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'y', 'z'];
+    if (value.toLowerCase().match(/[a-z]/i)) {
+        value = this.alphabet[this.alphabet.indexOf(value.toLowerCase()) - 1]
+        return value
+    } else {
+        value--
+        return value
+    }
+}
+
 class Piece {
-    constructor(name, color, originalPosition) {
+    constructor(name, colour, coordinate) {
         this.name = name;
-        this.color = color;
-        this.originalPosition = originalPosition;
+        this.colour = colour;
+        this.coordinate = coordinate;
     }
 
     toString() {
-        return `${this.name} ${this.color} ${this.originalPosition}`;
+        return `${this.name} ${this.colour} ${this.coordinate}`;
     }
 
-    potentialMoves() {}
+    moves() {}
 
-    move() {}
+    possibleMoves() {
+        let possibleCoordinates = []
+        this.moves().forEach((element) => {
+            if (Array.from(document.getElementById(element).children).length !== 0) {
+                if (!Array.from(Array.from(document.getElementById(element).children)[0].classList).includes(this.colour)) {
+                    if (Array.from(Array.from(document.getElementById(element).children)[0].classList).includes('king')) {
+                        /*
+                        set king to check
+                        Update: I believe this will never run, the king will never be open to any piece as it would be check.
+                        */
+                    } else {
+                        possibleCoordinates.push(element)
+                        console.log(possibleCoordinates, 'hi3')
+                    }
+                }
+            } else {
+                possibleCoordinates.push(element)
+            }
+        })
+        return possibleCoordinates
+    }
+
+    left(coordinate) {
+        return decrement(coordinate[0]) + coordinate[1]
+    }
+    right(coordinate) {
+        return increment(coordinate[0]) + coordinate[1]
+    }
+    up(coordinate) {
+        return coordinate[0] + increment(coordinate[1])
+    }
+    down(coordinate) {
+        return coordinate[0] + decrement(coordinate[1])
+    }
 
     create() {
-        if (!positions.includes(this.originalPosition)) return
+        if (!positions.includes(this.coordinate)) return
 
-        let position = document.getElementById(this.originalPosition)
+        let position = document.getElementById(this.coordinate)
         let piece = document.createElement('img');
 
         id += 1
         piece.id = String(id)
 
         piece.classList.add('pieces')
-        piece.src = `../../images/pieces/${this.color}/${this.name}.png`;
+        piece.classList.add(this.name)
+        piece.classList.add(this.colour)
+        piece.src = `../../images/pieces/${this.colour}/${this.name}.png`;
 
         position.appendChild(piece);
     }
 }
 
 class Rook extends Piece {
-    constructor(color, originalPosition) {
-        super('rook', color, originalPosition)
+    constructor(colour, coordinate) {
+        super('rook', colour, coordinate)
+    }
+
+    moves() {
+        let possibleCoordinates = []
+
+        let originalCoordinate = this.coordinate;
+        let newCoordinate = this.coordinate;
+
+        while (true) {
+            newCoordinate = this.left(newCoordinate)
+            if (positions.includes(newCoordinate)) {
+                if (!possibleCoordinates.includes(newCoordinate)) {
+                    possibleCoordinates.push(newCoordinate)
+                }
+
+                console.log('left', originalCoordinate, newCoordinate)
+
+                let element = document.getElementById(newCoordinate)
+                if (element.children.length > 0) {
+                    newCoordinate = originalCoordinate
+                    break
+                }
+            } else {
+                newCoordinate = originalCoordinate
+                break
+            }
+        }
+        while (true) {
+            newCoordinate = this.right(newCoordinate)
+            if (positions.includes(newCoordinate)) {
+                if (!possibleCoordinates.includes(newCoordinate)) {
+                    possibleCoordinates.push(newCoordinate)
+                }
+
+                let element = document.getElementById(newCoordinate)
+                if (element.children.length > 0) {
+                    newCoordinate = originalCoordinate
+                    break
+                }
+            } else {
+                newCoordinate = originalCoordinate
+                break
+            }
+        }
+        while (true) {
+            newCoordinate = this.up(newCoordinate)
+            if (positions.includes(newCoordinate)) {
+                if (!possibleCoordinates.includes(newCoordinate)) {
+                    possibleCoordinates.push(newCoordinate)
+                }
+
+                console.log('up', originalCoordinate, newCoordinate)
+
+                let element = document.getElementById(newCoordinate)
+                if (element.children.length > 0) {
+                    newCoordinate = originalCoordinate
+                    break
+                }
+            } else {
+                newCoordinate = originalCoordinate
+                break
+            }
+        }
+        while (true) {
+            newCoordinate = this.down(newCoordinate)
+            if (positions.includes(newCoordinate)) {
+                if (!possibleCoordinates.includes(newCoordinate)) {
+                    possibleCoordinates.push(newCoordinate)
+                }
+
+                console.log('down', originalCoordinate, newCoordinate)
+
+                let element = document.getElementById(newCoordinate)
+                if (element.children.length > 0) {
+                    newCoordinate = originalCoordinate
+                    break
+                }
+            } else {
+                newCoordinate = originalCoordinate
+                break
+            }
+        }
+        return possibleCoordinates
     }
 }
 
 class Knight extends Piece {
-    constructor(color, originalPosition) {
-        super('knight', color, originalPosition)
+    constructor(colour, coordinate) {
+        super('knight', colour, coordinate)
     }
 }
 
 class Bishop extends Piece {
-    constructor(color, originalPosition) {
-        super('bishop', color, originalPosition)
+    constructor(colour, coordinate) {
+        super('bishop', colour, coordinate)
     }
 }
 
 class Queen extends Piece {
-    constructor(color, originalPosition) {
-        super('queen', color, originalPosition)
+    constructor(colour, coordinate) {
+        super('queen', colour, coordinate)
     }
 }
 
 class King extends Piece {
-    constructor(color, originalPosition) {
-        super('king', color, originalPosition)
+    constructor(colour, coordinate) {
+        super('king', colour, coordinate)
     }
 }
 
 class Pawn extends Piece {
-    constructor(color, originalPosition) {
-        super('pawn', color, originalPosition)
+    constructor(colour, coordinate) {
+        super('pawn', colour, coordinate)
     }
 }
 
 function normalGame() {
-    let whiteRook = new Rook('white', 'a1');
+    let whiteRook = new Rook('white', 'a3');
     let whiteKnight = new Knight('white', 'b1');
     let whiteBishop = new Bishop('white', 'c1');
     let whiteQueen = new Queen('white', 'd1');
@@ -135,35 +275,42 @@ function normalGame() {
     blackPawn6.create();
     blackPawn7.create();
     blackPawn8.create();
+
+    console.log(whiteRook.moves())
+    console.log(whiteRook.possibleMoves())
+
+    let pieces = document.getElementsByClassName('pieces')
+    Array.from(pieces).forEach((element) => {
+        element.addEventListener('dragstart', (event) => {
+            event.dataTransfer.setData('text/plain', event.target.id);
+        })
+    })
+
+    Array.from(squares).forEach((element) => {
+        element.addEventListener('dragover', (event) => {
+            event.preventDefault();
+        });
+
+        element.addEventListener('drop', (event) => {
+            event.preventDefault();
+
+            let data = event.dataTransfer.getData('text/plain');
+            let element = document.getElementById(data)
+
+            if (element.id === event.target.id) return
+
+            if (!whiteRook.possibleMoves().includes(event.target.id)) return
+
+                if (event.target.tagName.toLowerCase() === 'img') {
+                    if (Array.from(event.target.classList).includes('king')) return
+
+                    event.target.parentNode.appendChild(element)
+                    event.target.remove()
+                } else {
+                    event.target.appendChild(element);
+                }
+        })
+    })
 }
 
 normalGame()
-
-let pieces = document.getElementsByClassName('pieces')
-Array.from(pieces).forEach((element) => {
-    element.addEventListener('dragstart', (event) => {
-        console.log(event.target.id)
-        console.log(typeof event.target.id)
-        event.dataTransfer.setData('text/plain', event.target.id);
-    })
-})
-
-let random = document.getElementById('a6')
-Array.from(squares).forEach((element) => {
-    element.addEventListener('dragover', (event) => {
-        event.preventDefault();
-    });
-
-    // blackRook.addEventListener('dragleave', (event) => {
-    //     event.preventDefault();
-    // });
-
-    element.addEventListener('drop', (event) => {
-        event.preventDefault();
-
-        let data = event.dataTransfer.getData('text/plain');
-        let element = document.getElementById(data)
-
-        event.target.appendChild(element);
-    })
-})
