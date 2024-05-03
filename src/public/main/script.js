19aacf89ebe58444ef8775bea0264e4c17902cc6
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2024-05-03 19:57:29 +0100

    bad update

--- src/public/main/script.js
+++ src/public/main/script.js
@@ -1,31 +1,123 @@
-const squares = document.getElementsByClassName('square')
-const positions = Array.from(squares, (square) => square.id)
+class Game {
+    constructor() {
+        this.players_turn = 'white'
+        this.GameOver = false
+        this.createBoard()
+        this.pieces = this.createDefaultPieces()
+        this.mainGame()
+    }
 
-function increment(value) {
-    this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'y', 'z'];
-    if (value.toLowerCase().match(/[a-z]/i)) {
-        value = this.alphabet[this.alphabet.indexOf(value.toLowerCase()) + 1]
-        if (!value) {
-            value = this.alphabet[0]
-        }
-        return value
-    } else {
-        value++
-        return value
+    createBoard() {
+        console.log('hi')
+    }
+
+    createDefaultPieces() {
+        let whiteRook = new Rook('white', 'a1');
+        let whiteKnight = new Knight('white', 'b1');
+        let whiteBishop = new Bishop('white', 'c1');
+        let whiteQueen = new Queen('white', 'd1');
+        let whiteKing = new King('white', 'e1');
+        let whiteBishop2 = new Bishop('white', 'f1');
+        let whiteKnight2 = new Knight('white', 'g1');
+        let whiteRook2 = new Rook('white', 'h1');
+        let whitePawn = new Pawn('white', 'a2');
+        let whitePawn2 = new Pawn('white', 'b2');
+        let whitePawn3 = new Pawn('white', 'c2');
+        let whitePawn4 = new Pawn('white', 'd2');
+        let whitePawn5 = new Pawn('white', 'e2');
+        let whitePawn6 = new Pawn('white', 'f2');
+        let whitePawn7 = new Pawn('white', 'g2');
+        let whitePawn8 = new Pawn('white', 'h2');
+        let blackRook = new Rook('black', 'a8');
+        let blackKnight = new Knight('black', 'b8');
+        let blackBishop = new Bishop('black', 'c8');
+        let blackQueen = new Queen('black', 'd8');
+        let blackKing = new King('black', 'e8');
+        let blackBishop2 = new Bishop('black', 'f8');
+        let blackKnight2 = new Knight('black', 'g8');
+        let blackRook2 = new Rook('black', 'h8');
+        let blackPawn = new Pawn('black', 'a7');
+        let blackPawn2 = new Pawn('black', 'b7');
+        let blackPawn3 = new Pawn('black', 'c7');
+        let blackPawn4 = new Pawn('black', 'd7');
+        let blackPawn5 = new Pawn('black', 'e7');
+        let blackPawn6 = new Pawn('black', 'f7');
+        let blackPawn7 = new Pawn('black', 'g7');
+        let blackPawn8 = new Pawn('black', 'h7');
+        return [whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop2, whiteKnight2, whiteRook2, whitePawn, whitePawn2, whitePawn3, whitePawn4, whitePawn5, whitePawn6, whitePawn7, whitePawn8, blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop2, blackKnight2, blackRook2, blackPawn, blackPawn2, blackPawn3, blackPawn4, blackPawn5, blackPawn6, blackPawn7, blackPawn8]
+    }
+
+    mainGame() {
+        let pieces = this.pieces
+
+        Array.from(pieces).forEach((piece) => {
+            piece.addEventListener('dragstart', (event) => {
+                event.dataTransfer.setData('text/plain', event.target.id);
+            })
+        })
+
+        Array.from(squares).forEach((square) => {
+            square.addEventListener('dragover', (event) => {
+                event.preventDefault();
+            });
+
+            square.addEventListener('drop', (event) => {
+                event.preventDefault();
+
+                let data = event.dataTransfer.getData('text/plain');
+                let element = document.getElementById(data)
+
+                if (element.id === event.target.id) return
+
+                console.log(getPieceFromId(element.id).getMoves(), 123)
+
+                if (event.target.tagName.toLowerCase() === 'img') {
+                    if (!getPieceFromId(element.id).getMoves().includes(square.id)) return
+
+                    if (Array.from(event.target.classList).includes('king')) return
+
+                    getPieceFromId(element.id).updateCoordinate(square.id)
+                    square.appendChild(element)
+                    event.target.remove()
+                } else {
+                    if (!getPieceFromId(element.id).getMoves().includes(event.target.id)) return
+                    getPieceFromId(element.id).updateCoordinate(square.id)
+                    event.target.appendChild(element);
+                }
+            })
+        })
     }
 }
 
-function decrement(value) {
-    this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'y', 'z'];
-    if (value.toLowerCase().match(/[a-z]/i)) {
-        value = this.alphabet[this.alphabet.indexOf(value.toLowerCase()) - 1]
-        if (!value) {
-            value = this.alphabet[this.alphabet.length - 1]
+class Method {
+    constructor() {
+        this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'y', 'z'];
+    }
+
+    increment(value) {
+        if (value.toLowerCase().match(/[a-z]/i)) {
+            value = this.alphabet[this.alphabet.indexOf(value.toLowerCase()) + 1]
+            if (!value) {
+                value = this.alphabet[0]
+            }
+            return value
+        } else {
+            value++
+            return value
         }
-        return value
-    } else {
-        value--
-        return value
+    }
+    
+    decrement(value) {
+        if (value.toLowerCase().match(/[a-z]/i)) {
+            value = this.alphabet[this.alphabet.indexOf(value.toLowerCase()) - 1]
+            if (!value) {
+                value = this.alphabet[this.alphabet.length - 1]
+            }
+            return value
+        } else {
+            value--
+            return value
+        }
     }
 }
 
@@ -38,6 +130,7 @@ class Piece {
         this.coordinate = coordinate;
         this.canBeKilled = false;
         this.id = Piece.id
+        this.method = new Method()
         this.create()
     }
 
@@ -52,16 +145,16 @@ class Piece {
     }
 
     left(coordinate) {
-        return decrement(coordinate[0]) + coordinate[1]
+        return this.method.decrement(coordinate[0]) + coordinate[1]
     }
     right(coordinate) {
-        return increment(coordinate[0]) + coordinate[1]
+        return this.method.increment(coordinate[0]) + coordinate[1]
     }
     up(coordinate) {
-        return coordinate[0] + increment(coordinate[1])
+        return coordinate[0] + this.method.increment(coordinate[1])
     }
     down(coordinate) {
-        return coordinate[0] + decrement(coordinate[1])
+        return coordinate[0] + this.method.decrement(coordinate[1])
     }
 
     create() {
@@ -434,104 +527,11 @@ function testGame() {
     })
 }
 
-
-function normalGame() {
-    let whiteRook = new Rook('white', 'a1');
-    let whiteKnight = new Knight('white', 'b1');
-    let whiteBishop = new Bishop('white', 'c1');
-    let whiteQueen = new Queen('white', 'd1');
-    let whiteKing = new King('white', 'e1');
-    let whiteBishop2 = new Bishop('white', 'f1');
-    let whiteKnight2 = new Knight('white', 'g1');
-    let whiteRook2 = new Rook('white', 'h1');
-    let whitePawn = new Pawn('white', 'a2');
-    let whitePawn2 = new Pawn('white', 'b2');
-    let whitePawn3 = new Pawn('white', 'c2');
-    let whitePawn4 = new Pawn('white', 'd2');
-    let whitePawn5 = new Pawn('white', 'e2');
-    let whitePawn6 = new Pawn('white', 'f2');
-    let whitePawn7 = new Pawn('white', 'g2');
-    let whitePawn8 = new Pawn('white', 'h2');
-    let blackRook = new Rook('black', 'a8');
-    let blackKnight = new Knight('black', 'b8');
-    let blackBishop = new Bishop('black', 'c8');
-    let blackQueen = new Queen('black', 'd8');
-    let blackKing = new King('black', 'e8');
-    let blackBishop2 = new Bishop('black', 'f8');
-    let blackKnight2 = new Knight('black', 'g8');
-    let blackRook2 = new Rook('black', 'h8');
-    let blackPawn = new Pawn('black', 'a7');
-    let blackPawn2 = new Pawn('black', 'b7');
-    let blackPawn3 = new Pawn('black', 'c7');
-    let blackPawn4 = new Pawn('black', 'd7');
-    let blackPawn5 = new Pawn('black', 'e7');
-    let blackPawn6 = new Pawn('black', 'f7');
-    let blackPawn7 = new Pawn('black', 'g7');
-    let blackPawn8 = new Pawn('black', 'h7');
-
-    let getPieceFromId = (id) => [whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop2, whiteKnight2, whiteRook2, whitePawn, whitePawn2, whitePawn3, whitePawn4, whitePawn5, whitePawn6, whitePawn7, whitePawn8, blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop2, blackKnight2, blackRook2, blackPawn, blackPawn2, blackPawn3, blackPawn4, blackPawn5, blackPawn6, blackPawn7, blackPawn8].filter((element) => String(element.id) === id)[0]
-
-    let pieces = document.getElementsByClassName('pieces')
-
-    Array.from(pieces).forEach((piece) => {
-        piece.addEventListener('dragstart', (event) => {
-            event.dataTransfer.setData('text/plain', event.target.id);
-        })
-    })
-
-    Array.from(squares).forEach((square) => {
-        square.addEventListener('dragover', (event) => {
-            event.preventDefault();
-        });
-
-        square.addEventListener('drop', (event) => {
-            event.preventDefault();
-
-            let data = event.dataTransfer.getData('text/plain');
-            let element = document.getElementById(data)
-
-            if (element.id === event.target.id) return
-
-            console.log(getPieceFromId(element.id).getMoves(), 123)
-
-            if (event.target.tagName.toLowerCase() === 'img') {
-                if (!getPieceFromId(element.id).getMoves().includes(square.id)) return
-
-                if (Array.from(event.target.classList).includes('king')) return
-
-                getPieceFromId(element.id).updateCoordinate(square.id)
-                square.appendChild(element)
-                event.target.remove()
-            } else {
-                if (!getPieceFromId(element.id).getMoves().includes(event.target.id)) return
-                getPieceFromId(element.id).updateCoordinate(square.id)
-                event.target.appendChild(element);
-            }
-        })
-    })
-}
-
+const squares = document.getElementsByClassName('square')
+const positions = Array.from(squares, (square) => square.id)
 const gameContainer = document.getElementById('game-container')
-const mutationObserver = new MutationObserver((mutations, observer) => {
-    console.log(mutations)
-    // if (observer.takeRecords().length === 0) return;
-
-    mutations.forEach((mutation) => {
-        if (mutation.type === 'childList') {
-            console.log(observer.addedNodes[0])
-        }
-    })
-});
-
-mutationObserver.observe(gameContainer, {
-    attributes: true,
-    childList: true,
-    subtree: true
-});
-// mutationObserver.disconnect();
+const game = new Game()
 
 gameContainer.addEventListener('contextmenu', (event) => {
     event.preventDefault()
 });
-
-normalGame()
