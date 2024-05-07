7d9aaf6f6c4cfae5270230823d1dcb5d42689470
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2024-05-08 00:18:13 +0100

    New beginning?!?

--- src/public/main/test.js
+++ src/public/main/test.js
@@ -53,39 +53,11 @@ class Game {
     }
     defaultGame() {
         let whiteRook = new Rook('white', 'a1');
-        let whiteKnight = new Knight('white', 'b1');
-        let whiteBishop = new Bishop('white', 'c1');
-        let whiteQueen = new Queen('white', 'd1');
         let whiteKing = new King('white', 'e1');
-        let whiteBishop2 = new Bishop('white', 'f1');
-        let whiteKnight2 = new Knight('white', 'g1');
         let whiteRook2 = new Rook('white', 'h1');
         let whitePawn = new Pawn('white', 'a2');
-        let whitePawn2 = new Pawn('white', 'b2');
-        let whitePawn3 = new Pawn('white', 'c2');
-        let whitePawn4 = new Pawn('white', 'd2');
-        let whitePawn5 = new Pawn('white', 'e2');
-        let whitePawn6 = new Pawn('white', 'f2');
-        let whitePawn7 = new Pawn('white', 'g2');
-        let whitePawn8 = new Pawn('white', 'h2');
-        let blackRook = new Rook('black', 'a8');
-        let blackKnight = new Knight('black', 'b8');
-        let blackBishop = new Bishop('black', 'c8');
-        let blackQueen = new Queen('black', 'd8');
-        let blackKing = new King('black', 'e8');
-        let blackBishop2 = new Bishop('black', 'f8');
-        let blackKnight2 = new Knight('black', 'g8');
-        let blackRook2 = new Rook('black', 'h8');
-        let blackPawn = new Pawn('black', 'a7');
-        let blackPawn2 = new Pawn('black', 'b7');
-        let blackPawn3 = new Pawn('black', 'c7');
-        let blackPawn4 = new Pawn('black', 'd7');
-        let blackPawn5 = new Pawn('black', 'e7');
-        let blackPawn6 = new Pawn('black', 'f7');
-        let blackPawn7 = new Pawn('black', 'g7');
-        let blackPawn8 = new Pawn('black', 'h7');
 
-        let piecesObjects = [whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop2, whiteKnight2, whiteRook2, whitePawn, whitePawn2, whitePawn3, whitePawn4, whitePawn5, whitePawn6, whitePawn7, whitePawn8, blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop2, blackKnight2, blackRook2, blackPawn, blackPawn2, blackPawn3, blackPawn4, blackPawn5, blackPawn6, blackPawn7, blackPawn8];
+        let piecesObjects = [whiteRook, whiteKing, whiteRook2, whitePawn];
         let getPieceById = (id) => piecesObjects.find((element) => String(element.id) === id);
         let pieces = Array.from(document.getElementsByClassName('piece'));
         let previousSquare;
